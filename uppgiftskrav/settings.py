dc0bd15c257a96bd28d635cca8df83817e2fee51
Staffan Malmgren <staffan.malmgren@gmail.com>
2014-01-30 19:08:47 +0100

    correct DATABASE_URL for local dev

--- uppgiftskrav/settings.py
+++ uppgiftskrav/settings.py
@@ -58,9 +58,7 @@ WSGI_APPLICATION = 'uppgiftskrav.wsgi.application'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES = {'default': dj_database_url.config(default="postgres://localhost/upppgiftskrav")}
-print DATABASES
-
+DATABASES = {'default': dj_database_url.config(default="postgres:///uppgiftskrav")}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
