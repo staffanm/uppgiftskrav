d6aa4df5877de66db26c996d65ad0ec093e50f78
Staffan Malmgren <staffan.malmgren@gmail.com>
2014-01-30 18:53:11 +0100

    some fixes

--- uppgiftskrav/settings.py
+++ uppgiftskrav/settings.py
@@ -58,7 +58,9 @@ WSGI_APPLICATION = 'uppgiftskrav.wsgi.application'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default="postgres://localhost/upppgiftskrav")}
+print DATABASES
+
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- uppgiftskrav/wsgi.py
+++ uppgiftskrav/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uppgiftskrav.settings")
 
-from django.core.wsgi import get_wsgi_applicationa
+from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
