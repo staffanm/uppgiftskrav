491843c7ead812d0c2bad18fd12fde98038bddf6
Staffan Malmgren <staffan.malmgren@gmail.com>
2014-02-01 23:17:42 +0100

    redirect from frontpage to /db/

--- uppgiftskrav/urls.py
+++ uppgiftskrav/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.views.generic import RedirectView
+
 urlpatterns = patterns('',
                        # Examples:
+                       url(r'^$', RedirectView.as_view(url="/db/")),
                        url(r'^db/', include('register.urls', namespace='register')),
                        url(r'^admin/', include(admin.site.urls))
                        )
