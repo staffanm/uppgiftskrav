862ae5963bf8bf6967c6dbaba3625cad0db380fc
Staffan Malmgren <staffan.malmgren@gmail.com>
2014-02-01 13:32:50 +0100

    basic views w templates for user-facing site

--- register/urls.py
+++ register/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import patterns, url
 from register import views
 
 urlpatterns = patterns('',
-                       url(r'^uppgift/$', views.uppgifter),
-                       url(r'^krav/$', views.kravlist),
-                       url(r'^krav/(?P<krav_id>\d+)$', views.krav),
+                       url(r'^uppgift/$', views.uppgiftlist, name="uppgiftlist"),
+                       url(r'^uppgift/(?P<uppgiftid>[A-Z]+\d+)$', views.uppgift, name="uppgift"),
+                       url(r'^krav/$', views.kravlist, name="kravlist"),
+                       url(r'^krav/(?P<kravid>[A-Z]+\d+)$', views.krav, name="krav"),
                        url(r'^$', views.index),
 )
--- register/views.py
+++ register/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
+from register.models import Uppgift, Krav
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the front page")
+    return render(request, "register/index.html")
 
 def kravlist(request):
-    return HttpResponse("Hello, world. You're looking at all krav")
-
-def krav(request, krav_id):
-    return HttpResponse("This is krav %s" % krav_id)
+    return render(request, "register/kravlist.html",
+                  {'krav_list':Krav.objects.order_by('kravid')})
 
 
-from register.models import Uppgift
-def uppgifter(request):
+def krav(request, kravid):
+    k = get_object_or_404(Krav, kravid=kravid)
+    return render(request, "register/krav.html", {'krav':k})
+
+
+def uppgift(request, uppgiftid):
+    u = get_object_or_404(Uppgift, uppgiftid=uppgiftid)
+    return render(request, "register/uppgift.html", {'uppgift':u})
+
+def uppgiftlist(request):
+    return render(request, "register/uppgiftlist.html",
+                  {'uppgift_list':Uppgift.objects.order_by('uppgiftid')})
     
-    return HttpResponse("Hello, world. You're looking at " + ', '.join([str(u) for u in Uppgift.objects.order_by('namn')]))
 
     
